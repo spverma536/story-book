007b2141448e36aedd687639eae41ee3f4eb0d82
spverma536 <spverma536@gmail.com>
2020-09-18 01:13:45 +0530

    changed json file

--- app.js
+++ app.js
@@ -79,4 +79,6 @@ app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.port || 3000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+//app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+app.listen(process.env.PORT || 5000)
